55d3da7c9cdae3f695dce7d423b2317d7cd312b7
jujia1987 <harry.xiao.zhou@gmail.com>
2019-11-15 01:41:56 +0000

    Replace debug in the setting.py

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -2,5 +2,6 @@
 cd /home/ec2-user/Dev/trydjango
 source bin/activate
 cd /home/ec2-user/Dev/trydjango/src
+sed -e 's/DEBUG = True/DEBUG = False/g' settings.py
 python3 manage.py makemigrations
 python3 manage.py migrate
