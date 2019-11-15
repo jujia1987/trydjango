8df20305bf0d31dd874925eff9d717561b1a5c9b
jujia1987 <harry.xiao.zhou@gmail.com>
2019-11-15 02:50:57 +0000

    change the stop_application

--- scripts/stop_application.sh
+++ scripts/stop_application.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
+#cd /home/ec2-user/Dev/trydjango
+#source bin/activate
+#cd /home/ec2-user/Dev/trydjango/src
+#kill $(ps aux |grep runserver | grep 'Sl+' | awk -F' ' '{print $2}')
+
+
 cd /home/ec2-user/Dev/trydjango
 source bin/activate
 cd /home/ec2-user/Dev/trydjango/src
-kill $(ps aux |grep runserver | grep 'Sl+' | awk -F' ' '{print $2}')
+pid=$(ps aux |grep runserver | grep 'Sl+' | awk -F' ' '{print $2}')
+if [ ! -z $pid]
+
+then kill $pid
+
+fi
+
