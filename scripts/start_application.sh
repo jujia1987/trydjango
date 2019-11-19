b9c9152b363840f0f24e452a8d929cab8f92bb82
jujia1987 <harry.xiao.zhou@gmail.com>
2019-11-19 21:49:40 +0000

    change the port from 8000 to 80

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -3,4 +3,4 @@ cd /home/ec2-user/Dev/trydjango
 source bin/activate
 cd /home/ec2-user/Dev/trydjango/src
 #nohup python3 manage.py runserver 0.0.0.0:8000 &
-python3 manage.py runserver 0.0.0.0:8000 > /dev/null 2>&1 &
+python3 manage.py runserver 0.0.0.0:80 > /dev/null 2>&1 &
