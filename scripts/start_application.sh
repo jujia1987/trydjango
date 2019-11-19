7591607c5047c002b14af3ecccff9083640eef2b
jujia1987 <harry.xiao.zhou@gmail.com>
2019-11-19 21:45:24 +0000

    update the appspec with stdout and stderr again

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -2,4 +2,5 @@
 cd /home/ec2-user/Dev/trydjango
 source bin/activate
 cd /home/ec2-user/Dev/trydjango/src
-nohup python3 manage.py runserver 0.0.0.0:8000 &
+#nohup python3 manage.py runserver 0.0.0.0:8000 &
+python3 manage.py runserver 0.0.0.0:8000 > /dev/null 2>&1 &
