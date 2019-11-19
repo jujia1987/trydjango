855e5c7a9ea9de1e6e1ab4615562ac4a8a48e27b
jujia1987 <harry.xiao.zhou@gmail.com>
2019-11-19 10:35:41 +0000

    update the migrate script

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -2,6 +2,6 @@
 cd /home/ec2-user/Dev/trydjango
 source bin/activate
 cd /home/ec2-user/Dev/trydjango/src/trydjango
-sed -e 's/DEBUG = True/DEBUG = False/g' settings.py
+sed -e 's/DEBUG = True/DEBUG = False/g' settings.py > settings.py #overwrite the file
 python3 manage.py makemigrations
 python3 manage.py migrate
