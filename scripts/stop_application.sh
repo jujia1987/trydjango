155e084425f4f9c9a2eadd4699c48a3d10013c9c
jujia1987 <harry.xiao.zhou@gmail.com>
2019-11-19 11:12:50 +0000

    modify stop script again again

--- scripts/stop_application.sh
+++ scripts/stop_application.sh
@@ -9,9 +9,9 @@ cd /home/ec2-user/Dev/trydjango
 source bin/activate
 cd /home/ec2-user/Dev/trydjango/src
 pid=$(ps aux |grep runserver | grep 'Sl' | awk -F' ' '{print $2}')
-if [ ! -z $pid]
-
-then kill $pid
-
+echo "$pid"
+if [ ! -z "$pid"]
+then 
+   kill "$pid"
 fi
 
