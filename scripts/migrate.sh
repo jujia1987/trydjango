dc6ebdbf684c476d537635a0fda630f144570523
jujia1987 <harry.xiao.zhou@gmail.com>
2019-11-19 11:07:17 +0000

    modify migrate.sh again

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -4,5 +4,6 @@ sed -e 's/DEBUG = True/DEBUG = False/g' settings.py > settings.py #overwrite the
 echo 'Start Migration'
 cd /home/ec2-user/Dev/trydjango
 source bin/activate
+cd /home/ec2-user/Dev/trydjango/src
 python3 manage.py makemigrations
 python3 manage.py migrate
